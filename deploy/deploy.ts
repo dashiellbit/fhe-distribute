0fc0c63b95220b07399d98d351da3b214df684d8
dashiellbit <ylhkvktrb978@hotmail.com>
2025-10-17 16:54:12 +0800

    deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,29 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  // Deploy FHECounter (kept for template completeness)
+  const deployedFHECounter = await deploy("FHECounter", { from: deployer, log: true });
+  console.log(`FHECounter contract: `, deployedFHECounter.address);
+
+  // Deploy ConfidentialETH
+  const deployedConfidentialETH = await deploy("ConfidentialETH", { from: deployer, log: true });
+  console.log(`ConfidentialETH contract: `, deployedConfidentialETH.address);
+
+  // Deploy Distributor with ConfidentialETH address
+  const deployedDistributor = await deploy("Distributor", {
     from: deployer,
+    args: [deployedConfidentialETH.address],
     log: true,
   });
+  console.log(`Distributor contract: `, deployedDistributor.address);
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  // Mint initial cETH balance to Distributor for distributions (example: 1,000,000 units)
+  const signer = await hre.ethers.getSigner(deployer);
+  const ceth = await hre.ethers.getContractAt("ConfidentialETH", deployedConfidentialETH.address, signer);
+  const mintTx = await ceth.mint(deployedDistributor.address, 1_000_000);
+  await mintTx.wait();
+  console.log(`Minted initial cETH to Distributor`);
 };
 export default func;
 func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.tags = ["FHECounter", "ConfidentialETH", "Distributor"];
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,16 +6,19 @@ import "@typechain/hardhat";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import type { HardhatUserConfig } from "hardhat/config";
-import { vars } from "hardhat/config";
+import * as dotenv from "dotenv";
+dotenv.config();
 import "solidity-coverage";
 
 import "./tasks/accounts";
 import "./tasks/FHECounter";
+import "./tasks/Distributor";
 
 // Run 'npx hardhat vars setup' to see the list of variables that need to be set
 
-const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
-const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+const MNEMONIC: string = process.env.MNEMONIC || "test test test test test test test test test test test junk";
+const INFURA_API_KEY: string = process.env.INFURA_API_KEY || "";
+const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -23,9 +26,7 @@ const config: HardhatUserConfig = {
     deployer: 0,
   },
   etherscan: {
-    apiKey: {
-      sepolia: vars.get("ETHERSCAN_API_KEY", ""),
-    },
+    apiKey: { sepolia: ETHERSCAN_API_KEY },
   },
   gasReporter: {
     currency: "USD",
